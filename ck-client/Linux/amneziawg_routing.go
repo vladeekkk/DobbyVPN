ff3ecc5fe84dbffb7dc88bf6e2db0e72a47ca9a9
nalek0 <belash.aleksandr777@gmail.com>
2025-01-02 19:31:17 +0300

    Linux tunnel service

--- ck-client/Linux/amneziawg_routing.go
+++ ck-client/Linux/amneziawg_routing.go
@@ -4,55 +4,20 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"os"
-	"strings"
 
-	"github.com/amnezia-vpn/amneziawg-go/conn"
-	"github.com/amnezia-vpn/amneziawg-go/device"
-	"github.com/amnezia-vpn/amneziawg-go/ipc"
-	"github.com/amnezia-vpn/amneziawg-go/tun"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 
 	sysctl "github.com/lorenzosaino/go-sysctl"
 )
 
-type AmneziaWGTunnel struct {
-	configFilePath      string
-	interfaceName       string
-	amneziawgDevice     *device.Device
-	amneziawgUAPISocket net.Listener
-	config              *Config
-}
+const tunnelServicePath = "libs/tunnel-service"
+const tunnelLogLevel = "debug"
 
-var tunnels = make(map[string]*AmneziaWGTunnel)
-
-func (amneziawgTunnel *AmneziaWGTunnel) Close() error {
-	err1 := amneziawgTunnel.amneziawgUAPISocket.Close()
-	amneziawgTunnel.amneziawgDevice.Close()
-
-	if err1 != nil {
-		return err1
-	} else {
-		return nil
-	}
-}
-
-func (amneziawgTunnel *AmneziaWGTunnel) configureInterface() error {
-	uapiString, err := amneziawgTunnel.config.ToUAPI()
-	if err != nil {
-		return err
-	}
-
-	reader := strings.NewReader(uapiString)
-
-	return amneziawgTunnel.amneziawgDevice.IpcSetOperation(reader)
-}
-
-func (amneziawgTunnel *AmneziaWGTunnel) setUpInterface() error {
-	link, err := netlink.LinkByName(amneziawgTunnel.interfaceName)
+func setUpInterface(configFilePath, interfaceName string) error {
+	link, err := netlink.LinkByName(interfaceName)
 	if err != nil {
 		return err
 	}
@@ -60,9 +25,9 @@ func (amneziawgTunnel *AmneziaWGTunnel) setUpInterface() error {
 	return netlink.LinkSetUp(link)
 }
 
-func (amneziawgTunnel *AmneziaWGTunnel) addAddresses() error {
-	for _, address := range amneziawgTunnel.config.Interface.Addresses {
-		if err := amneziawgTunnel.addAddress(address.String()); err != nil {
+func addAddresses(config *Config, interfaceName string) error {
+	for _, address := range config.Interface.Addresses {
+		if err := addAddress(interfaceName, address.String()); err != nil {
 			return err
 		}
 	}
@@ -70,9 +35,10 @@ func (amneziawgTunnel *AmneziaWGTunnel) addAddresses() error {
 	return nil
 }
 
-func (amneziawgTunnel *AmneziaWGTunnel) addAddress(address string) error {
+func addAddress(interfaceName, address string) error {
+	Logging.Info.Printf("Add address %s %s", interfaceName, address)
 	// sudo ip -4 address add <address> dev <interfaceName>
-	link, err := netlink.LinkByName(amneziawgTunnel.interfaceName)
+	link, err := netlink.LinkByName(interfaceName)
 	if err != nil {
 		return err
 	}
@@ -85,10 +51,10 @@ func (amneziawgTunnel *AmneziaWGTunnel) addAddress(address string) error {
 	return netlink.AddrAdd(link, addr)
 }
 
-func (amneziawgTunnel *AmneziaWGTunnel) addRoutes() error {
-	for _, peer := range amneziawgTunnel.config.Peers {
+func addRoutes(config *Config, interfaceName string) error {
+	for _, peer := range config.Peers {
 		for _, allowed_ip := range peer.AllowedIPs {
-			if err := amneziawgTunnel.addRoute(allowed_ip.String()); err != nil {
+			if err := addRoute(config, interfaceName, allowed_ip.String()); err != nil {
 				return err
 			}
 		}
@@ -97,12 +63,13 @@ func (amneziawgTunnel *AmneziaWGTunnel) addRoutes() error {
 	return nil
 }
 
-func (amneziawgTunnel *AmneziaWGTunnel) addRoute(address string) error {
+func addRoute(config *Config, interfaceName, address string) error {
+	Logging.Info.Printf("Add route for %s %s", interfaceName, address)
 	// sudo ip rule add not fwmark <table> table <table>
 	ruleNot := netlink.NewRule()
 	ruleNot.Invert = true
-	ruleNot.Mark = uint32(amneziawgTunnel.config.Interface.FwMark)
-	ruleNot.Table = int(amneziawgTunnel.config.Interface.FwMark)
+	ruleNot.Mark = uint32(config.Interface.FwMark)
+	ruleNot.Table = int(config.Interface.FwMark)
 	if err := netlink.RuleAdd(ruleNot); err != nil {
 		return err
 	}
@@ -116,7 +83,7 @@ func (amneziawgTunnel *AmneziaWGTunnel) addRoute(address string) error {
 	}
 
 	// sudo ip route add <address> dev <interfaceName> table <table>
-	link, err := netlink.LinkByName(amneziawgTunnel.interfaceName)
+	link, err := netlink.LinkByName(interfaceName)
 	if err != nil {
 		return err
 	}
@@ -126,7 +93,7 @@ func (amneziawgTunnel *AmneziaWGTunnel) addRoute(address string) error {
 		return err
 	}
 
-	route := netlink.Route{LinkIndex: link.Attrs().Index, Dst: dst, Table: 51820}
+	route := netlink.Route{LinkIndex: link.Attrs().Index, Dst: dst, Table: int(config.Interface.FwMark)}
 
 	if err := netlink.RouteAdd(&route); err != nil {
 		return err
@@ -140,117 +107,74 @@ func (amneziawgTunnel *AmneziaWGTunnel) addRoute(address string) error {
 	return nil
 }
 
-func NewAmneziaWGTunnel(configFilePath, interfaceName string) (*AmneziaWGTunnel, error) {
-	// open TUN device (or use supplied fd)
-	tunnelDevice, err := tun.CreateTUN(interfaceName, 1360)
+func runTunnelService(configFilePath, interfaceName string) error {
+	devNullFile, _ := os.Open(os.DevNull)
+	attr := &os.ProcAttr{
+		Files: []*os.File{
+			devNullFile, // stdin
+			os.Stdout,   // stdout
+			os.Stderr,   // stderr
+		},
+		Dir: ".",
+		Env: os.Environ(),
+	}
 
-	if err == nil {
-		realInterfaceName, err2 := tunnelDevice.Name()
-		if err2 == nil {
-			interfaceName = realInterfaceName
-		}
+	process, err := os.StartProcess(
+		tunnelServicePath,
+		[]string{
+			tunnelServicePath,
+			interfaceName,
+			configFilePath,
+			tunnelLogLevel,
+		},
+		attr,
+	)
+
+	if err != nil {
+		return err
 	} else {
-		return nil, fmt.Errorf("Error creating tunnel device: %s", err)
+		process.Release()
+		return nil
 	}
+}
 
-	// open UAPI file
-	fileUAPI, err := ipc.UAPIOpen(interfaceName)
-
+func readConfig(configFilePath, interfaceName string) (*Config, error) {
+	configData, err := os.ReadFile(configFilePath)
 	if err != nil {
-		return nil, fmt.Errorf("Error opening uapi for the tunnel: %s", err)
-	}
-
-	// Start device:
-	// Logger definition:
-	logLevel := device.LogLevelVerbose
-	logger := device.NewLogger(logLevel, fmt.Sprintf("(%s) ", interfaceName))
-	device := device.NewDevice(tunnelDevice, conn.NewDefaultBind(), logger)
-
-	errs := make(chan error)
-	uapi, err := ipc.UAPIListen(interfaceName, fileUAPI)
-	if err != nil {
-		device.Close()
 		return nil, err
 	}
 
-	go func() {
-		// Extra ipc configuration (for awg(8) util support):
-		for {
-			conn, err := uapi.Accept()
-			if err != nil {
-				errs <- err
-				return
-			}
-			go device.IpcHandle(conn)
-		}
-	}()
+	configDataString := string(configData)
 
-	configData, err := os.ReadFile(configFilePath)
-	if err != nil {
-		device.Close()
-		uapi.Close()
-		return nil, fmt.Errorf("Failed to read config file: %s", err)
-	}
+	Logging.Info.Printf("%s", configDataString)
 
-	config, err := FromWgQuickWithUnknownEncoding(string(configData), interfaceName)
-	if err != nil {
-		device.Close()
-		uapi.Close()
-		return nil, fmt.Errorf("Failed to parse config file: %s", err)
-	}
-
-	return &AmneziaWGTunnel{
-		configFilePath:      configFilePath,
-		interfaceName:       interfaceName,
-		amneziawgDevice:     device,
-		amneziawgUAPISocket: uapi,
-		config:              config,
-	}, nil
+	return FromWgQuickWithUnknownEncoding(configDataString, interfaceName)
 }
 
-func closeTunnel(interfaceName string) error {
-	tun := tunnels[interfaceName]
-	if tun != nil {
-		delete(tunnels, interfaceName)
-		return tun.Close()
-	}
-
-	return nil
-}
-
+// Installs AmneziaWG tunnel with the provided config and tunnel name
 func installTunnel(configFilePath, interfaceName string) error {
-	amneziawgTunnel, err := NewAmneziaWGTunnel(configFilePath, interfaceName)
+	err := runTunnelService(configFilePath, interfaceName)
 	if err != nil {
 		return err
 	} else {
 		Logging.Info.Printf("Tunnel initialisation success")
 	}
 
-	tunnels[interfaceName] = amneziawgTunnel
-
-	if err := amneziawgTunnel.configureInterface(); err != nil {
-		closeTunnel(interfaceName)
-		return err
-	} else {
-		Logging.Info.Printf("Config set success")
-	}
-
-	if amneziawgTunnel.setUpInterface(); err != nil {
-		closeTunnel(interfaceName)
+	if setUpInterface(configFilePath, interfaceName); err != nil {
 		return err
 	} else {
 		Logging.Info.Printf("Interface set up success")
 	}
 
-	if amneziawgTunnel.addAddresses(); err != nil {
-		closeTunnel(interfaceName)
+	config, err := readConfig(configFilePath, interfaceName)
+
+	if addAddresses(config, interfaceName); err != nil {
 		return err
 	} else {
 		Logging.Info.Printf("Interface addresses addition success")
 	}
 
-	if amneziawgTunnel.addRoutes(); err != nil {
-		closeTunnel(interfaceName)
+	if addRoutes(config, interfaceName); err != nil {
 		return err
 	} else {
 		Logging.Info.Printf("Interface routing initialisation success")
@@ -262,20 +186,8 @@ func installTunnel(configFilePath, interfaceName string) error {
 func uninstallTunnel(interfaceName string) error {
 	link, err := netlink.LinkByName(interfaceName)
 	if err != nil {
-		err = netlink.LinkDel(link)
+		return err
 	}
 
-	if err != nil {
-		Logging.Info.Printf("Tunnel is already stopped")
-	} else {
-		Logging.Info.Printf("Stop tunnel: %s", interfaceName)
-	}
-
-	if err := closeTunnel(interfaceName); err != nil {
-		Logging.Info.Printf("Error during interface close: %s", err)
-	}
-
-	Logging.Info.Printf("Interface closed")
-
-	return err
+	return netlink.LinkDel(link)
 }

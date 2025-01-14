317e75c0068c80d2012f944483dea9fbfe333cad
Brudlord <brudloord@yandex.ru>
2025-01-15 00:15:29 +0300

    Rewrite Bat to Kotlin

--- ck-client-desktop-kotlin/Client/src/jvmMain/kotlin/Main.kt
+++ ck-client-desktop-kotlin/Client/src/jvmMain/kotlin/Main.kt
@@ -10,28 +10,8 @@ fun main() = application {
     // Получение текущего пути к jar-файлу
     val encodedPath = this::class.java.protectionDomain.codeSource.location.path
     val decodedPath = URLDecoder.decode(encodedPath, StandardCharsets.UTF_8.name())
-    val appDir = File(decodedPath).parentFile // Директория приложения
-    val batFilePath = File(appDir, "add_tap_device.bat").absolutePath
-
-    val batFile = File(batFilePath)
-    if (batFile.exists()) {
-        try {
-            // Запускаем bat файл
-            val process = ProcessBuilder(batFile.absolutePath, appDir.absolutePath)
-                .inheritIO() // Для того чтобы вывод был виден в консоли
-                .start()
-
-            // Ожидаем завершения процесса
-            val exitCode = process.waitFor()
-            println("Process exited with code $exitCode")
-        } catch (e: IOException) {
-            println("Failed to start the process: ${e.message}")
-        } catch (e: InterruptedException) {
-            println("The process was interrupted: ${e.message}")
-        }
-    } else {
-        println("The file add_tap_device.bat does not exist in the current directory.")
-    }
+    val appDir = File(decodedPath).parentFile.absolutePath
+    addTapDevice(appDir)
 
     // Очистка логов при завершении приложения
     Runtime.getRuntime().addShutdownHook(Thread {
@@ -42,7 +22,33 @@ fun main() = application {
     })
 
     // Отображение основного окна приложения
-    Window(onCloseRequest = ::exitApplication, title = batFile.exists().toString()) {
+    Window(onCloseRequest = ::exitApplication, title = "Dobby VPN") {
         MainScreen()
+    }
+}
+
+fun startBat(appDir: String){
+    val batFilePath = File(appDir, "add_tap_device.bat").absolutePath
+    println("batFilePath:$batFilePath")
+    val batFile = File(batFilePath)
+    if (batFile.exists()) {
+        try {
+            val process = ProcessBuilder(
+                "powershell",
+                "-Command",
+                "Start-Process",
+                "-FilePath",
+                "\'$batFilePath\'",
+                "-Verb", "RunAs"
+            ).start()
+            val exitCode = process.waitFor()
+            println("Process exited with code $exitCode")
+        } catch (e: IOException) {
+            println("Failed to start the process: ${e.message}")
+        } catch (e: InterruptedException) {
+            println("The process was interrupted: ${e.message}")
+        }
+    } else {
+        println("The file add_tap_device.bat does not exist in the current directory.")
     }
 }
---
+++ ck-client-desktop-kotlin/Client/src/jvmMain/kotlin/tap/AddTapDevice.kt
@@ -0,0 +1,74 @@
+import java.io.File
+
+fun updatePath() {
+    val currentPath = System.getenv("PATH")
+    val newPath = "$currentPath;${System.getenv("SystemRoot")}\\system32;${System.getenv("SystemRoot")}\\system32\\wbem;${System.getenv("SystemRoot")}\\system32\\WindowsPowerShell\\v1.0"
+    System.setProperty("java.library.path", newPath)
+
+    println("Updated PATH: $newPath")
+}
+
+fun addTapDevice(appDir: String) {
+    val deviceName = "outline-tap0"
+    val deviceHwid = "tap0901"
+    val tapInstallPath = "tap-windows6/tapinstall.exe"
+    val oemVistaPath = "tap-windows6/OemVista.inf"
+
+    updatePath()
+    // Checking if a TAP device exists
+    if (executeCommand("netsh interface show interface name=$deviceName") == 0) {
+        println("TAP network device already exists.")
+        return
+    }
+    println("Creating TAP network device...")
+    runAsAdmin(appDir, "$tapInstallPath install $oemVistaPath $deviceHwid")
+
+    // Find new TAP device name (we should change it to outline-tap0)
+    val tapName = findTapDeviceName()
+    if (tapName.isNullOrEmpty()) {
+        println("Could not find TAP device name.")
+        return
+    }
+    println("Found TAP device name: $tapName")
+
+    // Rename TAP device
+    if (executeCommand("netsh interface set interface name=\"$tapName\" newname=\"$deviceName\"") != 0) {
+        println("Could not rename TAP device.")
+        return
+    }
+}
+
+fun executeCommand(command: String): Int {
+    return try {
+        val process = ProcessBuilder("cmd.exe", "/c", command).start()
+        process.waitFor()
+    } catch (e: Exception) {
+        println("Error executing command: $command\n${e.message}")
+        -1
+    }
+}
+
+
+fun runAsAdmin(appDir: String, command: String) {
+    val processBuilder = ProcessBuilder(
+        "powershell",
+        "-Command",
+        "Start-Process powershell -WindowStyle Hidden -ArgumentList \"-NoProfile;  $command\" -Verb RunAs"
+    )
+
+    try {
+        val process = processBuilder
+            .redirectErrorStream(true)
+            .directory(File(appDir))
+            .start()
+
+        process.inputStream.bufferedReader().use { reader ->
+            println(reader.readText())
+        }
+
+        process.waitFor()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+

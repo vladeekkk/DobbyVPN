5cc78583a3cef31acad6b746089685e4706a239b
Andrey Aksenov <aksenovandrey4@gmail.com>
2025-03-10 11:50:52 +0300

    Add cloak submodule, refactor cloak client

---
+++ go_client/cloak/client.go
@@ -0,0 +1,64 @@
+package cloak
+
+import (
+	"encoding/json"
+	"github.com/cbeuw/Cloak/exported_client"
+	log "github.com/sirupsen/logrus"
+	"sync"
+)
+
+var (
+	client *exported_client.CkClient
+	mu     sync.Mutex
+	once   sync.Once
+)
+
+func InitLog() {
+	exported_client.InitLog()
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+	log.SetLevel(log.InfoLevel)
+}
+
+func StartCloakClient(localHost, localPort, config string, udp bool) {
+	once.Do(InitLog)
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if client != nil {
+		log.Errorf("start cloak client failed: cloak client already started")
+		return
+	}
+
+	var rawConfig exported_client.Config
+	err := json.Unmarshal([]byte(config), &rawConfig)
+	if err != nil {
+		log.Errorf("cloak client: Failed to unmarshal config - %v", err)
+		return
+	}
+	log.Infof("cloak client: rawConfig parsed successfully: %+v", rawConfig)
+
+	rawConfig.LocalHost = localHost
+	rawConfig.LocalPort = localPort
+	rawConfig.UDP = udp
+	log.Infof("cloak client: rawConfig updated with LocalHost=%s, LocalPort=%s, UDP=%v", localHost, localPort, udp)
+
+	client = exported_client.NewCkClient(rawConfig)
+
+	client.Connect()
+}
+
+func StopCloakClient() {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if client == nil {
+		return
+	}
+
+	client.Disconnect()
+	client = nil
+}

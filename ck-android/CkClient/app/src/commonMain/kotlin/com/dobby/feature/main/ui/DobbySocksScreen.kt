d8b00693261ec848d425606bc670ef64f4d35600
nalek0 <belash.aleksandr777@gmail.com>
2025-03-17 22:25:15 +0300

    Outline page vertical center

--- ck-android/CkClient/app/src/commonMain/kotlin/com/dobby/feature/main/ui/DobbySocksScreen.kt
+++ ck-android/CkClient/app/src/commonMain/kotlin/com/dobby/feature/main/ui/DobbySocksScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -50,7 +51,9 @@ fun DobbySocksScreen(
     var apiKey by remember { mutableStateOf(uiState.outlineKey) }
 
     Column(
-        modifier = modifier.padding(16.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
         Row(

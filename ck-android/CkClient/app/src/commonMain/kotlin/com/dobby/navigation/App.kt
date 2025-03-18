73e700b68ae1430f9410cf78d283a29516d674b6
nalek0 <belash.aleksandr777@gmail.com>
2025-03-18 15:37:19 +0300

    Extracted bottom bar

--- ck-android/CkClient/app/src/commonMain/kotlin/com/dobby/navigation/App.kt
+++ ck-android/CkClient/app/src/commonMain/kotlin/com/dobby/navigation/App.kt
@@ -3,10 +3,11 @@ package com.dobby.navigation
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -27,6 +28,7 @@ import com.dobby.feature.logging.ui.LogScreen
 import com.dobby.feature.main.presentation.MainViewModel
 import com.dobby.feature.main.ui.AwgScreen
 import com.dobby.feature.main.ui.DobbySocksScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun App(
@@ -42,29 +44,7 @@ fun App(
                 detectTapGestures(onTap = { keyboardController?.hide() })
             },
         bottomBar = {
-            var selectedItem by remember { mutableIntStateOf(0) }
-            val items = listOf("Outline", "AmneziaWG", "Logs")
-            val screens = listOf(MainScreen, AmneziaWGScreen, LogsScreen)
-            val selectedIcons = listOf(Icons.Filled.Home, Icons.Filled.Favorite, Icons.Filled.Star)
-
-            NavigationBar {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        icon = {
-                            Icon(
-                                selectedIcons[index],
-                                contentDescription = item
-                            )
-                        },
-                        label = { Text(item) },
-                        selected = selectedItem == index,
-                        onClick = {
-                            selectedItem = index
-                            navController.navigate(screens[index])
-                        }
-                    )
-                }
-            }
+            BottomBar(navController::navigate)
         },
         content = { innerPadding ->
             NavHost(
@@ -84,4 +64,32 @@ fun App(
             }
         }
     )
+}
+
+@Composable
+private fun BottomBar(onNavigate: (Any) -> Unit = {}) {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Outline", "AmneziaWG", "Logs")
+    val screens = listOf(MainScreen, AmneziaWGScreen, LogsScreen)
+    val selectedIcons =
+        listOf(Icons.Filled.Home, Icons.Filled.Favorite, Icons.AutoMirrored.Filled.List)
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        selectedIcons[index],
+                        contentDescription = item
+                    )
+                },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    onNavigate.invoke(screens[index])
+                }
+            )
+        }
+    }
 }

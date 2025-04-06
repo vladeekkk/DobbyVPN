5301f1d232314f85e87b463a0fe73e56a1407fa3
Andrey Aksenov <aksenovandrey4@gmail.com>
2025-04-07 00:20:47 +0300

    Add todo

--- go_client/healthcheck/openvpn.go
+++ go_client/healthcheck/openvpn.go
@@ -15,10 +15,9 @@ type vpnHealth struct {
 func (s *Server) onUnhealthyVPN(ctx context.Context, lastErrMessage string) {
 	s.logger.Info("program has been unhealthy for " +
 		s.vpn.healthyWait.String() + ": restarting VPN (healthcheck error: " + lastErrMessage + ")")
-	s.logger.Info("👉 See https://github.com/qdm12/gluetun-wiki/blob/main/faq/healthcheck.md")
-	s.logger.Info("DO NOT OPEN AN ISSUE UNLESS YOU READ AND TRIED EACH POSSIBLE SOLUTION")
 	names := s.vpn.client.GetClientNames()
 	for _, name := range names {
+		// TODO: send error with client names to server
 		s.vpn.client.Refresh(name) // TODO: handle error
 	}
 	s.vpn.healthyWait += *s.config.VPN.Addition

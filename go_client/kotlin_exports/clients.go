d94ced7dad439f2796589e738751a7001d6bc043
Andrey Aksenov <aksenovandrey4@gmail.com>
2025-04-28 02:05:00 +0300

    Refactor kotlin exports

--- go_client/kotlin_exports/clients.go
+++ go_client/kotlin_exports/clients.go
@@ -3,12 +3,36 @@ package kotlin_exports
 import (
 	"go_client/cloak"
 	"go_client/outline"
+	"net"
 )
 
-type OutlineClient *outline.OutlineClient
+type OutlineClient struct {
+	*outline.OutlineClient
+}
 
-func NewOutlineClient(transportConfig string) OutlineClient {
-	return outline.NewClient(transportConfig)
+func (c *OutlineClient) Connect() error {
+	return c.OutlineClient.Connect()
+}
+
+func (c *OutlineClient) Disconnect() error {
+	return c.OutlineClient.Disconnect()
+}
+
+func (c *OutlineClient) GetServerIP() net.IP {
+	return c.OutlineClient.GetServerIP()
+}
+
+func (c *OutlineClient) Read() ([]byte, error) {
+	return c.OutlineClient.Read()
+}
+
+func (c *OutlineClient) Write(buf []byte) (int, error) {
+	return c.OutlineClient.Write(buf)
+}
+
+func NewOutlineClient(transportConfig string) *OutlineClient {
+	cl := outline.NewClient(transportConfig)
+	return &OutlineClient{OutlineClient: cl}
 }
 
 func StartCloakClient(localHost, localPort, config string, udp bool) {
